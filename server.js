dcfa3c2bda2fd458f9c2b089c254b2f97d89cafb
Dustin Evans <dustinaevans@gmail.com>
2016-03-08 17:00:26 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+// http://www.tutorialspoint.com/nodejs/nodejs_express_framework.htm
+
+var port = 80;
+var express = require('express');
+var app = express();
+app.listen(port);
+console.log("Magic happens on port " + port);
+
+var baseDir = "/users/dustine/desktop/node/public";
+
+app.use(express.static('public'));
+
+app.get('/bla', function (req, res) {
+    res.sendFile(baseDir + "/" + "index.html")
+    console.log("Sent index");
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send('Sorry cant find that!');
+});
+
+exports = module.exports = app;
